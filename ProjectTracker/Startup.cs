f67da5dd2d410d76a12bd57c8d5d9cbaa56c163c
Richard Kang <kangrc1@gmail.com>
2021-04-16 00:09:01 -0400

    Test branch protection

--- ProjectTracker/Startup.cs
+++ ProjectTracker/Startup.cs
@@ -12,6 +12,7 @@ namespace ProjectTracker
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Test
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
