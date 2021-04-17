6b756b28f43a21a9ba59835632af960c9fa47960
Richard Kang <kangrc1@gmail.com>
2021-04-17 14:50:55 -0400

    Data sent to backend

---
+++ ProjectTracker/wwwroot/js/Project/AddProject.js
@@ -0,0 +1,16 @@
+﻿document.querySelector('#displayAddProjectPartialBtn').addEventListener('click', (e) => {
+    $('#projectPartial').load('/Project/AddProjectPartial', function () {
+        addFormSubmitEvent()
+    });
+});
+
+function addFormSubmitEvent() {
+    $('#addProjectBtn').click(function () {
+
+        var dataToSend = $('#addProjectForm :input').serialize();
+
+        $.post('/Project/AddProject', dataToSend, function () {
+            $('#projectPartial').empty();
+        });
+    });
+}
