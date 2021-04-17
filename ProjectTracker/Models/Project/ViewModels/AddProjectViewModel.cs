e583dfdfe6447439adc6903a1f9d7b700211f7f0
Richard Kang <kangrc1@gmail.com>
2021-04-17 13:13:23 -0400

    Add project

---
+++ ProjectTracker/Models/Project/ViewModels/AddProjectViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectTracker.Models.Project.ViewModels
+{
+    public class AddProjectViewModel
+    {
+        public string Name { get; set; }
+
+        public string Status { get; set; }
+
+        public string Stage { get; set; }
+
+        public string Comments { get; set; }
+    }
+}
