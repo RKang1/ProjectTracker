2f158361d115b4331aa194543e98dde06da5d090
Richard Kang <kangrc1@gmail.com>
2021-04-17 11:58:38 -0400

    Dashboard Read works

---
+++ ProjectTracker/Models/Project/ProjectModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectTracker.Models.Project
+{
+    public class ProjectModel
+    {
+        public string Name { get; set; }
+
+        public string Status { get; set; }
+
+        public string Stage { get; set; }
+
+        public string Comments { get; set; }
+    }
+}
