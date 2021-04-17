0f8c8f6b461fee3b5eb0531d13c1f0b5d328af1e
Richard Kang <kangrc1@gmail.com>
2021-04-17 13:13:23 -0400

    Add project

---
+++ ProjectTracker/Controllers/ProjectController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectTracker.Models.Project.ViewModels;
+
+namespace ProjectTracker.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddProjectPartial()
+        {
+            return PartialView("~/Views/Project/Partials/AddProjectPartial.cshtml", new AddProjectViewModel());
+        }
+    }
+}
