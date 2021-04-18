12d112183a0c1785fd23a472b0f7049822532e08
Richard Kang <kangrc1@gmail.com>
2021-04-18 17:32:45 -0400

    Writing to database working

---
+++ ProjectTrackerDb/dbo/StoredProcecdures/PostDeployment/PopulateData/PopulateStatuses.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[PopulateStatuses]
+AS
+
+DECLARE
+	@Description NVARCHAR(50)
+
+SET @Description = 'Nothing needed';
+IF NOT EXISTS (select 1 from Statuses WHERE Description = @Description)
+BEGIN
+	INSERT INTO Statuses (Description) VALUES (@Description);
+END
+
+SET @Description = 'Needs Work';
+IF NOT EXISTS (select 1 from Statuses WHERE Description = @Description)
+BEGIN
+	INSERT INTO Statuses (Description) VALUES (@Description);
+END
+
+SET @Description = 'Waiting';
+IF NOT EXISTS (select 1 from Statuses WHERE Description = @Description)
+BEGIN
+	INSERT INTO Statuses (Description) VALUES (@Description);
+END
+
+SET @Description = 'Completed';
+IF NOT EXISTS (select 1 from Statuses WHERE Description = @Description)
+BEGIN
+	INSERT INTO Statuses (Description) VALUES (@Description);
+END
+
+RETURN 0
--- ProjectTrackerDb/dbo/Tables/Statuses.sql
+++ ProjectTrackerDb/dbo/Tables/Statuses.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[Statuses]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT IDENTITY (1, 1) NOT NULL PRIMARY KEY, 
     [Description] NVARCHAR(50) NULL
 )
