3e8f485b0e45fe09b674e6612bcd031115cc85fc
Richard Kang <kangrc1@gmail.com>
2021-04-21 23:19:14 -0400

    Edit button loading the partial

--- ProjectTracker/Controllers/ProjectController.cs
+++ ProjectTracker/Controllers/ProjectController.cs
@@ -9,10 +9,12 @@ namespace ProjectTracker.Controllers
     public class ProjectController : Controller
     {
         private readonly IConfiguration _configuration;
+        private readonly ProjectDAO dao;
 
         public ProjectController(IConfiguration configuration)
         {
             _configuration = configuration;
+            dao = new(_configuration);
         }
 
         public IActionResult Index()
@@ -27,18 +29,20 @@ namespace ProjectTracker.Controllers
 
         public IActionResult LoadEditProject(int projectId)
         {
-            return PartialView("~/Views/Project/Partials/ProjectPartial.cshtml", new ProjectViewModel() { Mode = "edit" });
+            ProjectModel project = dao.GetProject(projectId);
+            ProjectViewModel viewModel = project;
+            viewModel.Mode = "edit"; 
+
+            return PartialView("~/Views/Project/Partials/ProjectPartial.cshtml", viewModel);
         }
 
         public void AddProject(ProjectModel newProject)
         {
-            ProjectDAO dao = new(_configuration);
             dao.AddProject(newProject);
         }
 
         public void EditProject(ProjectModel editProject)
         {
-            ProjectDAO dao = new(_configuration);
             dao.AddProject(editProject);
         }
     }
--- ProjectTracker/Models/Project/ViewModels/ProjectViewModel.cs
+++ ProjectTracker/Models/Project/ViewModels/ProjectViewModel.cs
@@ -2,8 +2,30 @@
 
 namespace ProjectTracker.Models.Project.ViewModels
 {
-    public class ProjectViewModel : ProjectModel
+    public class ProjectViewModel
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Status { get; set; }
+
+        public int Stage { get; set; }
+
+        public string Comments { get; set; }
+
         public string Mode { get; set; }
+
+        public static implicit operator ProjectViewModel(ProjectModel project)
+        {
+            return new ProjectViewModel
+            {
+                Id = project.Id,
+                Name = project.Name,
+                Status = project.Status,
+                Stage = project.Stage,
+                Comments = project.Comments
+            };
+        }
     }
 }
