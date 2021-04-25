d6cd0f4d3f6bafa8e84c26cadad380fbf99140d0
Richard Kang <kangrc1@gmail.com>
2021-04-25 12:11:22 -0400

    Added links to project pages

--- ProjectTracker/Controllers/ProjectController.cs
+++ ProjectTracker/Controllers/ProjectController.cs
@@ -1,14 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using ProjectTracker.DAOs;
-using ProjectTracker.Models.Project.Models;
-using ProjectTracker.Models.Project.ViewModels;
 
 namespace ProjectTracker.Controllers
 {
     public class ProjectController : Controller
     {
-        public ViewResult Index()
+        public ViewResult Index(int projectId)
         {
             return View();
         }
