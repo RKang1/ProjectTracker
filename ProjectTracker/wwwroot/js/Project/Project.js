459dd0957a2fff0bf10fb761a3691e5efbf20b30
Richard Kang <kangrc1@gmail.com>
2021-05-05 21:41:49 -0400

    Hooked up task frontend with backend

--- ProjectTracker/wwwroot/js/Project/Project.js
+++ ProjectTracker/wwwroot/js/Project/Project.js
@@ -4,5 +4,59 @@
 
 function loadTaskTablePartial() {
     $('#taskTablePartial').load('LoadTaskTablePartial', function () {
+        addTableEventHandlers();
+    });
+}
+
+$('#addTaskBtn').on('click', function () {
+    loadTaskHelper('add');
+});
+
+function addTableEventHandlers() {
+    $('.editTaskBtn').on('click', function () {
+        loadTaskHelper('edit');
+    });
+
+    $('.deleteTaskBtn').on('click', function () {
+        loadTaskHelper('delete');
+    });
+}
+
+function loadTaskHelper(mode) {
+    let taskId;
+
+    if (mode === 'edit' || mode === 'delete') {
+        taskId = $(this).closest('tr').find('.taskId').val();
+    }
+
+    let dataToSend = {
+        'mode': mode,
+        'taskId': taskId
+    };
+
+    $('#taskPartial').load('LoadTaskPartial', dataToSend, function () {
+        submitTaskEvent();
+    });
+}
+
+function loadTaskPartial(mode, taskId) {
+    let dataToSend = {
+        'mode': mode,
+        'taskId': taskId
+    };
+
+    $('#taskPartial').load('/Project/LoadTaskPartial', dataToSend, function () {
+        submitTaskEvent();
+    });
+}
+
+function submitProjectEvent() {
+    $('#submitProjectBtn').click(function () {
+        let dataToSend = $('#projectForm :input').serializeArray();
+
+        $.post('/Project/SubmitProject', dataToSend, function () {
+            loadTablePartial();
+            $('#projectPartial').empty();
+        });
     });
 }
