2857ce3f0f7e1afed6e4fb2631961e40b4b926f3
Richard Kang <kangrc1@gmail.com>
2021-06-11 21:51:30 -0400

    Sql refactoring

--- ProjectTrackerDb/dbo/Tables/Tasks.sql
+++ ProjectTrackerDb/dbo/Tables/Tasks.sql
@@ -5,6 +5,6 @@
     [Description] NVARCHAR (250) NULL,
     [Status] INT NULL,
     [Comments] NVARCHAR(1000) NULL,
-    CONSTRAINT [FK_Tasks_Statuses] FOREIGN KEY ([Status]) REFERENCES [Statuses]([Id]),
-    CONSTRAINT [FK_Tasks_Projects] FOREIGN KEY ([ProjectId]) REFERENCES [Projects]([Id])
+    CONSTRAINT [FK_Tasks_Projects] FOREIGN KEY ([ProjectId]) REFERENCES [Projects]([Id]),
+    CONSTRAINT [FK_Tasks_Statuses] FOREIGN KEY ([Status]) REFERENCES [Statuses]([Id])
 )
